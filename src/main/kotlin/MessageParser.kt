310347df2185882dde90bb1a8bc04352735302f6
DiabloZ <Diabloz@me.com>
2024-02-24 14:05:33 +0300

    Перенастроил обработчик ответов

--- src/main/kotlin/MessageParser.kt
+++ src/main/kotlin/MessageParser.kt
@@ -4,13 +4,24 @@ object MessageParser {
 
 	private const val DELIMITER = "\n"
 	private val regexPoint = Regex("\\d+")
+
 	fun parse(message: String): Map<Int, VoteType> {
 		val points = message.split(DELIMITER)
 		val mapVotes = mutableMapOf<Int, VoteType>()
 		points.forEach { point ->
-			val answerNumber = regexPoint.find(point)?.groupValues?.firstOrNull()?.toIntOrNull()
-			val answerDigit = point.lastOrNull()?.digitToIntOrNull()
+
+			val answers = regexPoint
+				.findAll(point)
+				.toList()
+				.mapNotNull { it.groupValues.firstOrNull() }
+			val answerNumber = answers.firstOrNull()?.toIntOrNull()
+			val answerDigit = answers.lastOrNull()?.toIntOrNull()
+
 			if (answerNumber != null && answerDigit != null && answerNumber <= 24) {
+				if (answers.size > 2 || answers.size <= 1) {
+					mapVotes[answerNumber] = VoteType.ERROR
+					return@forEach
+				}
 				val voteType = when(answerDigit) {
 					VoteType.YES.voteNumber -> VoteType.YES
 					VoteType.NO.voteNumber -> VoteType.NO
@@ -33,7 +44,11 @@ fun main(){
 		"5-1\n" +
 		"6  2\n" +
 		"7   1\n" +
-		"23  2\n"
+		"19     \n" +
+		"20     5\n" +
+		"21   12 3\n" +
+		"23  2\n" +
+		"27  2\n"
 
 	MessageParser.parse(message).forEach {
 		Logger.printText("MessageParserTEST", it)
